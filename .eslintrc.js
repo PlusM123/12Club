fa3297f7061c3a2fa556c5527664c5744acd2fb9
WangshuXC <1294673511@qq.com>
2024-12-19 14:27:48 +0800

    feat: 工程初始化，主题初始化

---
+++ .eslintrc.js
@@ -0,0 +1,116 @@
+// ************* Core ESLint Rules ************
+const commonCoreRules = {
+  indent: ['error', 2], // 强制使用两个空格进行缩进
+  'comma-dangle': ['error', 'always-multiline'], // 强制在多行的对象和数组字面量中的最后一个元素后面使用逗号
+  'object-curly-spacing': ['error', 'always'], // 强制在对象字面量的大括号内使用空格
+  eqeqeq: ['error', 'always'], // 要求使用全等（===）和非全等（!==）运算符
+  'no-console': 'error', // 禁止使用 console 语句
+  'no-empty': 'error', // 禁止出现空的代码块
+  'no-useless-catch': 'error', // 禁止出现不做任何事情的 catch 块
+  'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0 }], // 限制连续空行的数量不能超过1
+  'max-params': ['error', 5], // 限制函数定义中参数的最大数量为5
+  'max-depth': ['error', 3], // 限制代码块嵌套的最大深度为3
+  'max-nested-callbacks': ['error', 3], // 限制回调函数嵌套的最大深度为3
+
+  // 注释的换行规则 （以下规则只适用于独立的注释，如果注释位于代码行的末尾，则不会生效）
+  'lines-around-comment': [
+    'error',
+    {
+      beforeBlockComment: false, // 在块注释（/* ... */）之前可以不用换行
+      afterBlockComment: false, // 在块注释（/* ... */）之后可以不用换行
+      beforeLineComment: true, // 在行注释（// ...）之前需要一个空行
+      afterLineComment: false, // 在行注释（// ...）之后不需要空行
+      allowBlockStart: true, // 在块的开始处允许没有空行
+      allowBlockEnd: false, // 在块的结束处不允许没有空行
+      allowObjectStart: true, // 在对象的开始处允许没有空行
+      allowObjectEnd: false, // 在对象的结束处不允许没有空行
+      allowArrayStart: true, // 在数组的开始处允许没有空行
+      allowArrayEnd: false, // 在数组的结束处不允许没有空行
+    },
+  ],
+
+  // 代码的换行规则（注意顺序）
+  'padding-line-between-statements': [
+    'error',
+    { blankLine: 'always', prev: ['function'], next: 'return' }, // 在大部分return语句之前需要空行
+
+    // 块语句与任何语句间必须换行（包括块语句）
+    { blankLine: 'always', prev: '*', next: 'block-like' },
+    { blankLine: 'always', prev: 'block-like', next: '*' },
+
+    // 声明语句与其他分声明语句间必须换行 (函数\表达式语句\if\for 除外)
+    { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+    {
+      blankLine: 'any',
+      prev: ['const', 'let', 'var'],
+      next: ['const', 'let', 'var', 'function', 'expression', 'if', 'for'],
+    },
+
+    // import与其他语句间必须换行
+    { blankLine: 'always', prev: 'import', next: '*' },
+    { blankLine: 'any', prev: 'import', next: 'import' },
+
+    // export与任何语句间必须换行（包括export）
+    { blankLine: 'always', prev: '*', next: 'export' },
+    { blankLine: 'always', prev: 'export', next: 'export' },
+
+    // CommonJS：require与其他语句间必须换行
+    { blankLine: 'always', prev: 'cjs-import', next: '*' },
+    { blankLine: 'any', prev: 'cjs-import', next: 'cjs-import' },
+
+    // CommonJS：export与任何语句间必须换行（包括export）
+    { blankLine: 'always', prev: '*', next: 'cjs-export' },
+    { blankLine: 'always', prev: 'cjs-export', next: 'cjs-export' },
+  ],
+  'import/order': [
+    'error',
+    {
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+      pathGroups: [
+        {
+          pattern: 'react',
+          group: 'external',
+          position: 'before',
+        },
+      ],
+      pathGroupsExcludedImportTypes: ['react'],
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc',
+      },
+    },
+  ],
+
+  'no-restricted-syntax': [
+    'error',
+    {
+      selector: 'ForInStatement',
+      message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+    },
+    {
+      selector: 'LabeledStatement',
+      message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+    },
+    {
+      selector: 'WithStatement',
+      message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+    },
+
+    // 禁止使用 JSON.parse 或 JSON.stringify
+    {
+      selector: 'CallExpression[callee.object.name=\'JSON\'][callee.property.name=\'parse\']',
+      message: '请使用公共方法 safeJsonParse 替代 JSON.parse',
+    },
+    {
+      selector: 'CallExpression[callee.object.name=\'JSON\'][callee.property.name=\'stringify\']',
+      message: '请使用公共方法 safeJsonStringify 替代 JSON.stringify',
+    },
+  ],
+};
+
+module.exports = {
+  extends: ['next/core-web-vitals', 'next/typescript'],
+  rules: {
+    ...commonCoreRules,
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,26 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundColor: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+        foreground: "rgb(var(--color-foreground) / <alpha-value>)",
+        separator: "rgb(var(--color-separator) / <alpha-value>)",
+      },
+      textColor: {
+        primary: "rgb(var(--color-foreground) / <alpha-value>)",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
