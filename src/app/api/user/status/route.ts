fbc828db50ec775479818d3d59d1fb8f93fa3a2a
WangshuXC <1294673511@qq.com>
2025-09-29 00:52:06 +0800

    fix: 修复无法登陆的问题

--- src/app/api/user/status/route.ts
+++ src/app/api/user/status/route.ts
@@ -25,11 +25,6 @@ export const getStatus = async (uid: number | undefined) => {
       }
     })
 
-    window?.umami?.identify(user.id.toString(), {
-      name: user.name,
-      email: user.email,
-    })
-
     const responseData: UserState = {
       uid: user.id,
       name: user.name,
