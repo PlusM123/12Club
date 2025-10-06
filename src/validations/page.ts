d12ae0e055904103f4b6fdaa0ab4b919735ec90d
plusm <1040748815@qq.com>
2025-10-07 00:21:25 +0800

    feature: 页面筛选添加依发行时间排序，并规范名称

--- src/validations/page.ts
+++ src/validations/page.ts
@@ -10,7 +10,8 @@ export const pageSchema = z.object({
     z.literal('view'),
     z.literal('download'),
     z.literal('favorite_by'),
-    z.literal('comment')
+    z.literal('comment'),
+    z.literal('released')
   ]),
   sortOrder: z.union([z.literal('asc'), z.literal('desc')]),
   page: z.coerce.number().min(1).max(9999999),
